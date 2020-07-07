be6e98dd3d700e22e00a6220ba7e3918cb53f085
Henrik Thoroe <henrik@thoroee.com>
2020-07-07 17:00:55 +0200

    Vector2D Setup

---
+++ app/include/util/Vector2D.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+namespace Util {
+
+    /// The mathematical representation of a two dimensional vector (not C++ array like vector).
+    class Vector2D {
+        public:
+            /// @brief The x offset.
+            int x;
+
+            /// @brief The y offset.
+            int y;
+
+            Vector2D(int x, int y);
+            Vector2D(const Vector2D* other);
+            Vector2D(const Vector2D& other);
+
+            /// Scales the vector instance by the given factor.
+            void scale(const int& factor);
+
+            /// Shifts the vector with the passed vector. Equal to addtion of the x and y value of both vectors.
+            void shift(const Vector2D& offset);
+
+            /// The length of the vector.
+            int length() const;
+
+            /// Similiar to scale(const int&) but does not mutate the instance.
+            Vector2D scaled(const int& factor) const;
+
+            /// Similiar to shift(const Vector2D&) but does not mutate the instance.
+            Vector2D shifted(const Vector2D& offset) const;
+    };
+
+}
---
+++ app/src/util/Vector2D.cpp
@@ -0,0 +1,38 @@
+#include "Vector2D.hpp"
+#include <math.h>
+
+namespace Util {
+
+    Vector2D::Vector2D(int x, int y) : x(x), y(y) {};
+
+    Vector2D::Vector2D(const Vector2D& other) : x(other.x), y(other.y) {};
+
+    Vector2D::Vector2D(const Vector2D* other) : x(other->x), y(other->y) {};
+
+    void Vector2D::scale(const int& factor) {
+        x = factor * x;
+        y = factor * y;
+    }
+
+    void Vector2D::shift(const Vector2D& other) {
+        x = x + other.x;
+        y = y + other.y;
+    }
+
+    Vector2D Vector2D::scaled(const int& factor) const {
+        Vector2D scaled = new Vector2D(this);
+        scaled.scale(factor);
+        return scaled;
+    }
+
+    Vector2D Vector2D::shifted(const Vector2D& offset) const {
+        Vector2D shifted = new Vector2D(this);
+        shifted.shift(offset);
+        return shifted;
+    }
+
+    int Vector2D::length() const {
+        return sqrt(pow(x, 2) + pow(y, 2));
+    }
+
+}
