7ee21d4720f2b9c61d921584324e350f0158282e
Henrik Thoroe <henrik@thoroee.com>
2020-07-08 17:40:48 +0200

    Removed 'new' Keyword

--- app/src/util/Vector2D.cpp
+++ app/src/util/Vector2D.cpp
@@ -20,13 +20,13 @@ namespace Util {
     }
 
     Vector2D Vector2D::scaled(const int& factor) const {
-        Vector2D scaled = new Vector2D(this);
+        Vector2D scaled = Vector2D(this);
         scaled.scale(factor);
         return scaled;
     }
 
     Vector2D Vector2D::shifted(const Vector2D& offset) const {
-        Vector2D shifted = new Vector2D(this);
+        Vector2D shifted = Vector2D(this);
         shifted.shift(offset);
         return shifted;
     }
