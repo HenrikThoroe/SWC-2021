03aff52d7fcc47d9b16af5b48c0c098b61062a05
Henrik Thoroe <henrik@thoroee.com>
2020-07-08 12:08:32 +0200

    Move Method

--- app/include/util/Position.hpp
+++ app/include/util/Position.hpp
@@ -6,7 +6,11 @@ namespace Util {
 
     class Position: public Vector2D {
         public:
+            Position(int x, int y);
+
             bool isNextTo(const Position& other) const;
+
+            Position move(const Vector2D& direction) const;
     };
 
 }
--- app/src/util/Position.cpp
+++ app/src/util/Position.cpp
@@ -2,6 +2,8 @@
 
 namespace Util {
 
+    Position::Position(int x, int y) : Vector2D { x, y } {}
+
     bool Position::isNextTo(const Position& other) const {
         const int minX = x - 1;
         const int maxX = x + 1;
@@ -9,6 +11,10 @@ namespace Util {
         const int maxY = y + 1;
 
         return other.x >= minX && other.x <= maxX && other.y >= minY && other.y <= maxY;
+    }
+
+    Position Position::move(const Vector2D& direction) const {
+        return Position(x + direction.x, y + direction.y);
     }
 
 }
