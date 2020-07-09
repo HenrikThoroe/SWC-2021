6563b2d24e9c37db7762d334693d950f3a48a1d6
Henrik Thoroe <henrik@thoroee.com>
2020-07-09 10:13:26 +0200

    Occupied + Attach + Validation Points

--- app/include/model/DeployedPiece.hpp
+++ app/include/model/DeployedPiece.hpp
@@ -13,19 +13,33 @@ namespace Model {
     class DeployedPiece {
         public:
             /// The id of the corresenponding piece.
-            uint8_t pieceId;
+            const uint8_t pieceId;
 
             /// The origin of the piece. 
-            Util::Position origin;
+            const Util::Position origin;
 
             /// The rotation in which the piece has been deployed.
-            Rotation rotation;
+            const Rotation rotation;
 
             DeployedPiece(uint8_t id, Util::Position origin, Rotation rotation);
             DeployedPiece(const DeployedPiece& other);
             DeployedPiece(const DeployedPiece* other);
 
-            std::vector<Util::Position> getOccupiedPositions() const;
+            const std::vector<Util::Position>& getOccupiedPositions() const;
+
+            const std::vector<Util::Position>& getAttachPoints() const;
+
+            const std::vector<Util::Position>& getValidationPoints() const;
+
+        private:
+            /// A set of all positions which are occupied by the piece.
+            std::vector<Util::Position> occupiedPositions;
+
+            /// A set of all positions where other pieces with the same color could be attached.
+            std::vector<Util::Position> attachPoints;
+
+            /// A set of all positions where another piece of the same color is expected. If this is not the case, the deployed piece is invalid.
+            std::vector<Util::Position> validationPoints;
     };
 
 }
--- app/src/model/DeployedPiece.cpp
+++ app/src/model/DeployedPiece.cpp
@@ -1,22 +1,39 @@
 #include "DeployedPiece.hpp"
+#include "Position.hpp"
 
 namespace Model {
 
-    DeployedPiece::DeployedPiece(uint8_t id, Util::Position origin, Rotation rotation) : pieceId(id), origin(origin), rotation(rotation) {}
-
-    DeployedPiece::DeployedPiece(const DeployedPiece& other) : pieceId(other.pieceId), origin(other.origin), rotation(other.rotation) {}
-
-    DeployedPiece::DeployedPiece(const DeployedPiece* other) : pieceId(other->pieceId), origin(other->origin), rotation(other->rotation) {}
-
-    std::vector<Util::Position> DeployedPiece::getOccupiedPositions() const {
-        std::vector<Util::Position> positions;
+    DeployedPiece::DeployedPiece(uint8_t id, Util::Position origin, Rotation rotation) : pieceId(id), origin(origin), rotation(rotation) {
         Piece piece = Piece::allPieces.at(pieceId);
         Piece::Shape shape = std::get<0>(piece.rotations.at(uint8_t(rotation)));
+        Piece::AttachPoints attachPoints = std::get<1>(piece.rotations.at(uint8_t(rotation)));
 
         for (auto path : shape) {
-            positions.push_back(origin.move(path));
+            occupiedPositions.emplace_back(origin.move(path));
         }
 
-        return positions;
+        for (auto attachPointPath : attachPoints.at(0)) {
+            attachPoints.emplace_back(origin.move(attachPointPath));
+        }
+
+        for (auto validationPointPath : attachPoints.at(1)) {
+            validationPoints.emplace_back(origin.move(validationPointPath));
+        }
+    }
+
+    DeployedPiece::DeployedPiece(const DeployedPiece& other) : DeployedPiece(other.pieceId, other.origin, other.rotation) {}
+
+    DeployedPiece::DeployedPiece(const DeployedPiece* other) : DeployedPiece(other->pieceId, other->origin, other->rotation) {}
+
+    const std::vector<Util::Position>& DeployedPiece::getOccupiedPositions() const {
+        return occupiedPositions;
+    }
+
+    const std::vector<Util::Position>& DeployedPiece::getAttachPoints() const {
+        return attachPoints;
+    }
+
+    const std::vector<Util::Position>& DeployedPiece::getValidationPoints() const {
+        return validationPoints;
     }
 }
