da5dd0dec7403163c10e74ed9525353c24712280
Henrik Thoroe <henrik@thoroee.com>
2020-07-28 18:13:49 +0200

    Setup

---
+++ app/include/model/GameState.hpp
@@ -0,0 +1,61 @@
+#pragma once 
+
+#include <inttypes.h>
+#include <stack>
+#include <map>
+
+#include "Board.hpp"
+#include "Player.hpp"
+#include "DeployedPiece.hpp"
+
+namespace Model {
+
+    class GameState {
+        private:
+            /// Collection of all available players.
+            const std::array<Player, 2> players;
+
+            /// A stack (first-in first-out) which tracks the performed moves.
+            std::stack<DeployedPiece> performedMoves;
+
+            /// A cache to improve move calculation speed.
+            /// @warning Hashtable implementation will be changed to a more performant one.
+            std::map<uint64_t, std::vector<DeployedPiece>> moves;
+
+            /// A board instance which stores the state of the deloyed pieces.
+            Board board;
+
+            /// The current turn. 
+            uint8_t turn;
+
+        public:
+            GameState();
+            GameState(GameState* other) = delete;
+            GameState(GameState& other) = delete;
+
+            /// Returns the current turn.
+            const uint8_t& getTurn() const;
+
+            /// Returns the player which is eligible to perform the next move.
+            const Player& getCurrentPlayer() const;
+
+            /// Returns the color which may be deployed next.
+            const PieceColor& getCurrentPieceColor() const;
+
+            /// Finds and returns the possible moves on the current game board.
+            const std::vector<Move>& getPossibleMoves() const;
+
+            /// Performs the given move on the board.
+            void performMove(const Move& move);
+
+            /// Reverts the last performed move.
+            void revertLastMove();
+
+            /// Hashes the current state.
+            uint64_t hash() const;
+
+            /// Evaluates the current state.
+            int evaluate() const;
+    };
+
+}
---
+++ app/src/model/GameState.cpp
@@ -0,0 +1,46 @@
+#include "GameState.hpp"
+
+namespace Model {
+
+    GameState::GameState() : turn(0), players({ Player(PlayerColor::RED), Player(PlayerColor::BLUE) }) {}
+
+    const uint8_t& GameState::getTurn() const {
+        return turn;
+    }
+
+    const Player& GameState::getCurrentPlayer() const {
+        return players[turn % 2];
+    }
+
+    const PieceColor& GameState::getCurrentPieceColor() const {
+        const int idx = turn % 4;
+        return getCurrentPlayer().getPieceColors().at(idx > 1 ? idx - 2 : idx);
+    }
+
+    void GameState::performMove(const Move& move) {
+        board.dropPiece(move);
+        performedMoves.push(move);
+        turn += 1;
+    }
+
+    void GameState::revertLastMove() {
+        DeployedPiece& piece = performedMoves.top();
+
+        board.removePiece(piece);
+        performedMoves.pop();
+        turn -= 1;
+    }
+
+    const std::vector<Move>& GameState::getPossibleMoves() const {
+        return {};
+    }
+
+    uint64_t GameState::hash() const {
+        return 0;
+    }
+
+    int GameState::evaluate() const {
+        return 0;
+    }
+
+}
