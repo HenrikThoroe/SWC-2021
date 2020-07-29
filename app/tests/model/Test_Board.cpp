9285666f8bb2a32de980efc20b39152bc91f9290
Henrik Thoroe <henrik@thoroee.com>
2020-07-29 21:54:26 +0200

    Can drop pieces

--- app/tests/model/Test_Board.cpp
+++ app/tests/model/Test_Board.cpp
@@ -10,16 +10,16 @@ TEST_CASE("Test Board", "[model]") {
     auto piece2 = DeployedPiece(20, Position(10,12), Rotation::PI, PieceColor::YELLOW);
     auto piece3 = DeployedPiece(0, Position(19,0), Rotation::ZERO, PieceColor::BLUE);
 
-    board.dropPiece(piece1);
-    board.dropPiece(piece2);
-    board.dropPiece(piece3);
+    SECTION("Can Drop Pieces") {
+        board.dropPiece(piece1);
+        board.dropPiece(piece2);
+        board.dropPiece(piece3);
 
-    WARN(board);
-
-    REQUIRE(board.fields[0][0] == uint8_t(PieceColor::BLUE));
-    REQUIRE(board.fields[12][10] == uint8_t(PieceColor::YELLOW));
-    REQUIRE(board.fields[9][12] == uint8_t(PieceColor::YELLOW));
-    REQUIRE(board.fields[9][13] == uint8_t(PieceColor::YELLOW));
-    REQUIRE(board.fields[9][11] == uint8_t(PieceColor::YELLOW));
-    REQUIRE(board.fields[9][10] == uint8_t(PieceColor::YELLOW));
+        REQUIRE(board.at(0 ,  0) == PieceColor::BLUE);
+        REQUIRE(board.at(10, 12) == PieceColor::YELLOW);
+        REQUIRE(board.at(10, 11) == PieceColor::YELLOW);
+        REQUIRE(board.at(11, 11) == PieceColor::YELLOW);
+        REQUIRE(board.at(9 , 11) == PieceColor::YELLOW);
+        REQUIRE(board.at(8 , 11) == PieceColor::YELLOW);
+    }
 }
