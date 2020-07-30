392fc815dac382da086117c1cfdd9473639ce3ba
Henrik Thoroe <henrik@thoroee.com>
2020-07-30 17:04:11 +0200

    Add drop positions tests

--- app/tests/model/Test_Board.cpp
+++ app/tests/model/Test_Board.cpp
@@ -24,6 +24,26 @@ TEST_CASE("Test Board", "[model]") {
         REQUIRE(board.at(8 , 11) == PieceColor::YELLOW);
     }
 
+    SECTION("Has Valid Drop Positions") {
+        auto dropPositionsBlue = board.getDropPositions(PieceColor::BLUE);
+        auto dropPositionsYellow = board.getDropPositions(PieceColor::YELLOW);
+
+        REQUIRE_THROWS(board.getDropPositions(PieceColor::NONE));
+        REQUIRE(board.canDrop(PieceColor::BLUE, 1, 1));
+        REQUIRE_FALSE(board.canDrop(PieceColor::BLUE, 1, 0));
+        REQUIRE(board.canDrop(PieceColor::BLUE, 18, 1));
+
+        REQUIRE(board.canDrop(PieceColor::YELLOW, 9, 13));
+        REQUIRE(board.canDrop(PieceColor::YELLOW, 11, 13));
+        REQUIRE(board.canDrop(PieceColor::YELLOW, 7, 12));
+        REQUIRE(board.canDrop(PieceColor::YELLOW, 7, 10));
+        REQUIRE(board.canDrop(PieceColor::YELLOW, 12, 12));
+        REQUIRE(board.canDrop(PieceColor::YELLOW, 12, 10));
+
+        REQUIRE(dropPositionsBlue.size() == 2);
+        REQUIRE(dropPositionsYellow.size() == 6);
+    }
+
     board.removePiece(piece1);
     board.removePiece(piece2);
     board.removePiece(piece3);
@@ -36,5 +56,24 @@ TEST_CASE("Test Board", "[model]") {
         REQUIRE(board.at(11, 11) == PieceColor::NONE);
         REQUIRE(board.at(9 , 11) == PieceColor::NONE);
         REQUIRE(board.at(8 , 11) == PieceColor::NONE);
+    }
+
+    SECTION("Has Valid Drop Positions After Removing") {
+        auto dropPositionsBlue = board.getDropPositions(PieceColor::BLUE);
+        auto dropPositionsYellow = board.getDropPositions(PieceColor::YELLOW);
+
+        REQUIRE_FALSE(board.canDrop(PieceColor::BLUE, 1, 1));
+        REQUIRE_FALSE(board.canDrop(PieceColor::BLUE, 1, 0));
+        REQUIRE_FALSE(board.canDrop(PieceColor::BLUE, 18, 1));
+
+        REQUIRE_FALSE(board.canDrop(PieceColor::YELLOW, 9, 13));
+        REQUIRE_FALSE(board.canDrop(PieceColor::YELLOW, 11, 13));
+        REQUIRE_FALSE(board.canDrop(PieceColor::YELLOW, 7, 12));
+        REQUIRE_FALSE(board.canDrop(PieceColor::YELLOW, 7, 10));
+        REQUIRE_FALSE(board.canDrop(PieceColor::YELLOW, 12, 12));
+        REQUIRE_FALSE(board.canDrop(PieceColor::YELLOW, 12, 10));
+
+        REQUIRE(dropPositionsBlue.size() == 0);
+        REQUIRE(dropPositionsYellow.size() == 0);
     }
 }
