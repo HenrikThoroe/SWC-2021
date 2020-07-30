9e730446c045a4ebb43649b6450cf6b3ec06b7ca
Henrik Thoroe <henrik@thoroee.com>
2020-07-30 09:48:50 +0200

    Test remove piece

--- app/tests/model/Test_Board.cpp
+++ app/tests/model/Test_Board.cpp
@@ -10,16 +10,31 @@ TEST_CASE("Test Board", "[model]") {
     auto piece2 = DeployedPiece(20, Position(10,12), Rotation::PI, PieceColor::YELLOW);
     auto piece3 = DeployedPiece(0, Position(19,0), Rotation::ZERO, PieceColor::BLUE);
 
-    SECTION("Can Drop Pieces") {
-        board.dropPiece(piece1);
-        board.dropPiece(piece2);
-        board.dropPiece(piece3);
+    board.dropPiece(piece1);
+    board.dropPiece(piece2);
+    board.dropPiece(piece3);
 
+    SECTION("Can Drop Pieces") {
         REQUIRE(board.at(0 ,  0) == PieceColor::BLUE);
+        REQUIRE(board.at(19,  0) == PieceColor::BLUE);
         REQUIRE(board.at(10, 12) == PieceColor::YELLOW);
         REQUIRE(board.at(10, 11) == PieceColor::YELLOW);
         REQUIRE(board.at(11, 11) == PieceColor::YELLOW);
         REQUIRE(board.at(9 , 11) == PieceColor::YELLOW);
         REQUIRE(board.at(8 , 11) == PieceColor::YELLOW);
+    }
+
+    board.removePiece(piece1);
+    board.removePiece(piece2);
+    board.removePiece(piece3);
+
+    SECTION("Can Remove Piece") {
+        REQUIRE(board.at(0 ,  0) == PieceColor::NONE);
+        REQUIRE(board.at(19,  0) == PieceColor::NONE);
+        REQUIRE(board.at(10, 12) == PieceColor::NONE);
+        REQUIRE(board.at(10, 11) == PieceColor::NONE);
+        REQUIRE(board.at(11, 11) == PieceColor::NONE);
+        REQUIRE(board.at(9 , 11) == PieceColor::NONE);
+        REQUIRE(board.at(8 , 11) == PieceColor::NONE);
     }
 }
