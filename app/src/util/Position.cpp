b517758784a454e14a9fa81543ed26e956878892
Henrik Thoroe <henrik@thoroee.com>
2020-08-03 22:23:09 +0200

    Return before variable declaration

--- app/src/util/Position.cpp
+++ app/src/util/Position.cpp
@@ -5,14 +5,14 @@ namespace Util {
     Position::Position(int x, int y) : Vector2D { x, y } {}
 
     bool Position::isNextTo(const Position& other) const {
+        if (other == *this) {
+            return false;
+        }
+
         const int minX = x - 1;
         const int maxX = x + 1;
         const int minY = y - 1;
         const int maxY = y + 1;
-
-        if (other == *this) {
-            return false;
-        }
 
         return other.x >= minX && other.x <= maxX && other.y >= minY && other.y <= maxY;
     }
