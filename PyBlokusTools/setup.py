992742b87ebd7eaac9ec79994b488928b1d199d4
Rubin Raithel <33808743+Coronon@users.noreply.github.com>
2020-08-09 03:18:24 +0200

    (Scripts) make PyBlokusTools installable and make blokustest script available

--- PyBlokusTools/setup.py
+++ PyBlokusTools/setup.py
@@ -1,7 +1,23 @@
-from setuptools import setup, find_packages # type: ignore
-setup(
+import setuptools # type: ignore
+
+from pyblokustools.version import VERSION
+
+setuptools.setup(
     name="PyBlokusTools",
-    version="0.1",
-    packages=find_packages(),
-    package_data={'pyblokustools': ['data/*.json']}
+    version=VERSION,
+    author="Rubin Raithel, Henrik Thøre",
+    author_email="dev@rubinraithel.de",
+    description="Tools for the SWC-2021 Blokus project",
+    url="https://github.com/HenrikThoroe/SWC-2021/tree/master/scripts",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS",
+    ],
+    python_requires='>=3.8',
+    entry_points = {
+        "console_scripts": ['blokustest = pyblokustools.scripts.runTests:runTests']
+        },
 )
