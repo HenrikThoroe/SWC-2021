dfd98797b2c1e8a2e63ce5636931440ac80a4573
Henrik <henrik@thoroee.com>
2020-08-22 17:20:11 +0200

    Add hash tests

--- app/tests/model/Test_GameState.cpp
+++ app/tests/model/Test_GameState.cpp
@@ -31,7 +31,9 @@ TEST_CASE("Test Game State", "[model]") {
         REQUIRE(state.getCurrentPieceColor() == PieceColor::BLUE);
     }
 
-    SECTION("Can Hash") {
+    SECTION("Can Create and Revert Hashes") {
+        std::stack<uint64_t> hashes {};
+
         for (int x = 0; x < 30; ++x) {
             std::vector<const Move*> moves = state.getPossibleMoves();
 
@@ -42,13 +44,45 @@ TEST_CASE("Test Game State", "[model]") {
             int index = rand() % moves.size();
 
             state.performMove(*moves[index]);
-
-            WARN(state.hash());
+            hashes.push(state.hash());
         }
 
         for (int x = 0; x < 30; ++x) {
+            REQUIRE(hashes.top() == state.hash());
+            hashes.pop();
             state.revertLastMove();
-            WARN(state.hash());
+        }
+    }
+
+    SECTION("Has no Collisions") {
+        std::unordered_map<uint64_t, std::bitset<800>> map {};
+
+        for (int it = 0; it < 10000; ++it) {
+            for (int x = 0; x < 25; ++x) {
+                std::vector<const Move*> moves = state.getPossibleMoves();
+
+                if (moves.size() == 0) {
+                    FAIL(state);
+                }
+
+                int index = rand() % moves.size();
+
+                state.performMove(*moves[index]);
+                
+                if (map.find(state.hash()) == map.end()) {
+                    map[state.hash()] = state.uniqueHash();
+                } else {
+                    INFO(state);
+                    INFO(state.hash());
+                    INFO(map[state.hash()]);
+                    INFO(state.uniqueHash());
+                    REQUIRE(map[state.hash()] == state.uniqueHash());
+                }
+            }
+
+            for (int x = 0; x < 25; ++x) {
+                state.revertLastMove();
+            }
         }
     }
 }
