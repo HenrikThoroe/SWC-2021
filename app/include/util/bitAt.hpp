a5da014aee639c8d32172fb0e6f7537cd9c46a13
Henrik <henrik@thoroee.com>
2020-09-05 18:47:23 +0200

    Add doc-comment

--- app/include/util/bitAt.hpp
+++ app/include/util/bitAt.hpp
@@ -2,6 +2,10 @@
 
 namespace Util {
 
+    /**
+     * @brief Returns the bit at `pos` of `number`
+     * @throw Throws an exception if `pos` is not in range 0 ..< sizeof(number) * 8
+     */
     template<typename T, int size = sizeof(T)> T bitAt(T number, int pos) {
         if (pos < 0 || pos >= size * 8) {
             throw std::invalid_argument("Index not in range.");
