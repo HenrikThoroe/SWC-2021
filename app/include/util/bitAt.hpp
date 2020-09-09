d8cce25dddd269375862f35c4d49dfe0b1d793b7
Henrik Thoroe <henrik@thoroee.com>
2020-09-09 17:21:46 +0200

    Validate input only in debug mode

--- app/include/util/bitAt.hpp
+++ app/include/util/bitAt.hpp
@@ -4,12 +4,15 @@ namespace Util {
 
     /**
      * @brief Returns the bit at `pos` of `number`
-     * @throw Throws an exception if `pos` is not in range 0 ..< sizeof(number) * 8
+     * @throw Throws an exception in debug mode, if `pos` is not in range 0 ..< sizeof(number) * 8
      */
     template<typename T, int size = sizeof(T)> T bitAt(T number, int pos) {
+        
+        #ifdef DEBUG
         if (pos < 0 || pos >= size * 8) {
             throw std::invalid_argument("Index not in range.");
         }
+        #endif
 
         return static_cast<T>(~(number >> ((size * 8 - 1) - pos))) % 2 == 0;
     }
