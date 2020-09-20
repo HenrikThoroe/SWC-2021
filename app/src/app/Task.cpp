8fa22bd1e482f33c9b30d2bfec77343befd7e2a1
Rubin Raithel <33808743+Coronon@users.noreply.github.com>
2020-09-20 23:40:02 +0200

    (AppLayer) add backgroundTask interface

---
+++ app/src/app/Task.cpp
@@ -0,0 +1,21 @@
+#include <atomic>
+
+#include "Task.hpp"
+
+namespace App
+{
+    int Task::run(std::atomic<bool>& flag) {
+        while (true) {
+            // Check if message was recieved and task has to pause
+            if (flag) return 1;
+
+            try {
+                // Run task iteration and return if done
+                if (_run()) return 0;
+            } catch (...) {
+                // Catch all exceptions and let task return with exception
+                return 2;
+            }
+        }
+    }
+} // namespace App
