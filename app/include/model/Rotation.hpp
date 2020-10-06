b0a1e7a02c1f9aaf5b4575841a5ebd4d05208148
Henrik <henrik@thoroee.com>
2020-10-06 11:04:55 +0200

    Add flipped rotations

--- app/include/model/Rotation.hpp
+++ app/include/model/Rotation.hpp
@@ -4,13 +4,17 @@
 
 namespace Model {
 
-    /// An enum describing the rotation of a piece in radians.
+    /// An enum describing the rotation of a piece in radians. 
     /// @note Rotation maps to uint8_t
     enum class Rotation: uint8_t {
         ZERO = 0, 
         ONEHALFPI = 1,
         PI = 2, 
-        THREEHALFPI = 3
+        THREEHALFPI = 3,
+        ZERO_FLIPPED = 4, 
+        ONEHALFPI_FLIPPED = 5,
+        PI_FLIPPED = 6, 
+        THREEHALFPI_FLIPPED = 7
     };
 
 }
