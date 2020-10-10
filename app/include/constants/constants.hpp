0d8f4fd9f39dc65ac9d3bb3b4f8495aad3c3dd3a
Henrik <henrik@thoroee.com>
2020-10-10 18:47:27 +0200

    Add global constants

---
+++ app/include/constants/constants.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Constants {
+
+    constexpr int BOARD_SIZE = 400;
+
+    constexpr int BOARD_COLUMNS = 20;
+
+    constexpr int BOARD_ROWS = 20;
+
+    constexpr int PIECE_VARIANTS = 268800;
+
+    constexpr int PIECE_VARIANTS_NO_COLOR = 67200;
+
+    constexpr int TURNS = 100;
+
+    constexpr int PIECE_SHAPES = 21;
+
+}
