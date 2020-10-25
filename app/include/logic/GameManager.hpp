213a753b74cb0dfb69880e0cb1351ff8a06d652e
Rubin Raithel <33808743+Coronon@users.noreply.github.com>
2020-10-25 15:33:28 +0100

    (Logic) add GameManager blueprint

---
+++ app/include/logic/GameManager.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <vector>
+
+#include "Message.hpp"
+#include "Move.hpp"
+#include "color.hpp"
+
+namespace Logic {
+
+    /// Class that manages the GameState and handels MoveRequests
+    class GameManager {
+        private:
+            /// Pointer to XMLParsers`s vector of PieceColors still in the game
+            const std::vector<Model::PieceColor>* const colorsInGame;
+
+            /// Own PlayerColor
+            Model::PlayerColor ownColor;
+
+        public:
+            /**
+             * @brief Construct a new GameManager instance
+             * 
+             * @param colorsInGame Pointer to a vector of PieceColors still in the game
+             */
+            GameManager(const std::vector<Model::PieceColor>* const colorsInGame);
+
+            /// No copy constructors allowed
+            GameManager(GameManager* other) = delete;
+            GameManager(GameManager& other) = delete;
+
+            /**
+             * @brief Set own PlayerColor
+             * 
+             * @param color PlayerColor to use
+             */
+            void setColor(const Model::PlayerColor& color);
+
+            /**
+             * @brief Update own GameState with MementoMessage send by server
+             * 
+             * @param memento Memento received from server
+             */
+            void updateWithMemento(const App::MementoMsg& memento);
+
+            /**
+             * @brief Called when the server requests a move
+             * 
+             * @returns Const ptr to move the logic deemed best
+             */
+            const Model::Move* moveRequest();
+    };
+
+} // namespace Logic
---
+++ app/src/logic/GameManager.cpp
@@ -0,0 +1,21 @@
+#include "GameManager.hpp"
+
+namespace Logic {
+    
+    GameManager::GameManager(const std::vector<Model::PieceColor>* const colorsInGame) : colorsInGame(colorsInGame) {}
+
+    void GameManager::setColor(const Model::PlayerColor& color) {
+        ownColor = color;
+    }
+
+    void GameManager::updateWithMemento(const App::MementoMsg& memento) {
+        /* code */
+    }
+
+    const Model::Move* GameManager::moveRequest() {
+        // This will throw a ptr error when dereferenced (Just so test compilation works)
+        Model::Move m = Model::Move(0, Util::Position(0, 0), Model::Rotation::ZERO, Model::PieceColor::NONE);
+        return &m;
+    }
+
+} // namespace Logic
