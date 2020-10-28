1f4dbad00fa43154988a4afa5a1fd9f7a7b1189a
Rubin Raithel <33808743+Coronon@users.noreply.github.com>
2020-10-28 19:12:43 +0100

    (AppLayer) remove redundant public flags from struct and add constructor comment

--- app/include/app/Message.hpp
+++ app/include/app/Message.hpp
@@ -10,78 +10,81 @@
 namespace App {
     
     struct Message {
-        public:
-            /**
-             * @brief Construct a new Message
-             * 
-             * @param type Type of message
-             * @param payload Payload of message of any type (requires casting on access)
-             */
-            Message(MsgType type, std::any payload);
+        /**
+         * @brief Construct a new Message
+         * 
+         * @param type Type of message
+         * @param payload Payload of message of any type (requires casting on access)
+         */
+        Message(MsgType type, std::any payload);
 
 
-            /// Type of message
-            MsgType type;
+        /// Type of message
+        MsgType type;
 
-            /// Payload of message of any type (requires casting on access with std::any_cast<T>(i.payload))
-            std::any payload;
+        /// Payload of message of any type (requires casting on access with std::any_cast<T>(i.payload))
+        std::any payload;
     };
 
     struct MementoMsg {
-        public:
+        /**
+         * @brief Construct a new MementoMsg
+         * 
+         * @param startPiece id of startPiece
+         * @param lastMove Last move played on the board
+         * @param currentTurn Current turn
+         */
+        MementoMsg(uint8_t startPiece, std::optional<uint32_t> lastMove, uint8_t currentTurn);
 
-            MementoMsg(uint8_t startPiece, std::optional<uint32_t> lastMove, uint8_t currentTurn);
 
+        /// Index for PieceCollection pointing at the piece that has to be played in the first round
+        uint8_t startPiece;
 
-            /// Index for PieceCollection pointing at the piece that has to be played in the first round
-            uint8_t startPiece;
+        /// Index for 'allPieces' cache in GameState of last move on the field
+        std::optional<uint32_t> lastMove;
 
-            /// Index for 'allPieces' cache in GameState of last move on the field
-            std::optional<uint32_t> lastMove;
-
-            /// Current turn on the board
-            uint8_t currentTurn;
+        /// Current turn on the board
+        uint8_t currentTurn;
     };
 
     struct ResultMsg {
-        public:
-            /// An enum of different game end causes.
-            /// @note types map to uint8_t
-            enum class ResultCause: uint8_t {
-                REGULAR        = 0, 
-                LEFT           = 1,
-                RULE_VIOLATION = 2, 
-                SOFT_TIMEOUT   = 3,
-                HARD_TIMEOUT   = 4
+        /// An enum of different game end causes.
+        /// @note types map to uint8_t
+        enum class ResultCause: uint8_t {
+            REGULAR        = 0, 
+            LEFT           = 1,
+            RULE_VIOLATION = 2, 
+            SOFT_TIMEOUT   = 3,
+            HARD_TIMEOUT   = 4
 
-            };
+        };
 
-            /// An enum of different game endings.
-            /// @note types map to uint8_t
-            enum class ResultEnd: uint8_t {
-                LOSE = 0,
-                DRAW = 1,
-                WIN  = 2
-            };
+        /// An enum of different game endings.
+        /// @note types map to uint8_t
+        enum class ResultEnd: uint8_t {
+            LOSE = 0,
+            DRAW = 1,
+            WIN  = 2
+        };
 
-            /**
-             * @brief Construct a new ResultMsg
-             * 
-             * @param score GameScore both players reached
-             * @param end TournamentPoints both players earned
-             * @param cause Game-ending causes for both players
-             */
-            ResultMsg(std::array<uint8_t, 2> score, std::array<ResultEnd, 2> end, std::array<ResultCause, 2> cause);
+        /**
+         * @brief Construct a new ResultMsg
+         * 
+         * @param score GameScore both players reached
+         * @param end TournamentPoints both players earned
+         * @param cause Game-ending causes for both players
+         */
+        ResultMsg(std::array<uint8_t, 2> score, std::array<ResultEnd, 2> end, std::array<ResultCause, 2> cause);
 
 
-            /// Array that holds both players inGame score reached
-            std::array<uint8_t, 2>     score;
+        /// Array that holds both players inGame score reached
+        std::array<uint8_t, 2>     score;
 
-            /// Array that holds both players tournamentPoints earned in this round
-            std::array<ResultEnd, 2>   end;
+        /// Array that holds both players tournamentPoints earned in this round
+        std::array<ResultEnd, 2>   end;
 
-            /// Array that holds both players game-ending causes
-            std::array<ResultCause, 2> cause;
+        /// Array that holds both players game-ending causes
+        std::array<ResultCause, 2> cause;
     };
     
 }
