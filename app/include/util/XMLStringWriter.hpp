4a9108326f8f321e8a638126b728896a1ee66656
Rubin Raithel <33808743+Coronon@users.noreply.github.com>
2020-10-28 19:45:50 +0100

    (AppLayer) fix styling of XMLStringWriter

--- app/include/util/XMLStringWriter.hpp
+++ app/include/util/XMLStringWriter.hpp
@@ -3,6 +3,7 @@
 #include "pugixml.hpp"
 
 namespace Util {
+
     /// Utility struct used for converting XMLs to string with pugixml.
     struct XMLStringWriter : pugi::xml_writer {
         /// The string that contains the XML.
@@ -14,8 +15,9 @@ namespace Util {
          * @param data The XML data.
          * @param size The size of the XML data.
          */
-        virtual void write(const void *data, size_t size) {
+        virtual void write(const void* data, size_t size) {
             result.append(static_cast<const char *>(data), size);
         }
     };
-} // namespace Util
+
+}
