155212e445143509a544b8e07978d8730f7c93e0
Rubin Raithel <33808743+Coronon@users.noreply.github.com>
2020-11-15 19:00:48 +0100

    (Scripts+TestServer) let GameManager log all events and fix multiple games issues

--- PyBlokusTools/pyblokustools/testserver/GameManager.py
+++ PyBlokusTools/pyblokustools/testserver/GameManager.py
@@ -2,9 +2,11 @@ from typing import Any, Final, Tuple, List
 
 from subprocess import Popen, PIPE, STDOUT
 import time # Remove once the server is fixed
+import logging
 
 from .TCPClient import TCPClient
 from .XMLParser import XMLParser
+from .Logger import Logger
 from .Message import *
 
 class GameManager():
@@ -12,15 +14,21 @@ class GameManager():
 
     Arguments:
         serverClient    {TCPClient}                   -- TCPClient already connected and authenticated
+        serverPort      {int}                         -- Port the clients should connect to
         clients         {Tuple[str, str]}             -- Paths to the two clients to test
         clientNames     {Tuple[str, str]}             -- Names for the two clients
         clientArguments {Tuple[List[str], List[str]]} -- Arguments to start clients with
     """
-    def __init__(self, serverClient: TCPClient, clients: Tuple[str, str], clientNames: Tuple[str, str], clientArguments: Tuple[List[str], List[str]]) -> None:
+    def __init__(self, serverClient: TCPClient, serverPort: int, clients: Tuple[str, str], clientNames: Tuple[str, str], clientArguments: Tuple[List[str], List[str]], logger: Logger) -> None:
         self.serverClient    = serverClient
+        self.serverPort      = str(serverPort)
         self.clients         = clients
         self.clientNames     = clientNames
         self.clientArguments = clientArguments
+        self.logger          = logger
+        
+        self._l              = logging.getLogger("TestServer.GameManager")
+        
         self.playedGames     = 0 # Number of games already played (to switch client colors)
     
     def run(self) -> ResultMsg:
@@ -34,8 +42,8 @@ class GameManager():
         reservations = self._prepareGame()
         
         # Start clients
-        client1 = Popen([self.clients[0], '-r', reservations[0] if self.playedGames % 2 == 0 else reservations[1], *self.clientArguments[0]], stdout=PIPE, stderr=STDOUT)
-        client2 = Popen([self.clients[1], '-r', reservations[1] if self.playedGames % 2 == 0 else reservations[0], *self.clientArguments[1]], stdout=PIPE, stderr=STDOUT)
+        client1 = Popen([self.clients[0], '-p', self.serverPort, '-r', reservations[0] if self.playedGames % 2 == 0 else reservations[1], *self.clientArguments[0]], stdout=PIPE, stderr=STDOUT)
+        client2 = Popen([self.clients[1], '-p', self.serverPort, '-r', reservations[1] if self.playedGames % 2 == 0 else reservations[0], *self.clientArguments[1]], stdout=PIPE, stderr=STDOUT)
         
         time.sleep(0.5) # See Bugreport on Discord -> Server is overwhelmed lmao
         self.serverClient.send(f'<pause roomId="{self._roomId}" pause="false" />') # Start game
@@ -50,21 +58,66 @@ class GameManager():
                     if msg.type == MsgType.GAMESTATE:
                         self._turn = msg.payload.currentTurn
                     elif msg.type == MsgType.RESULT:
+                        self.playedGames += 1
+                        
                         # Wait for clients to shutdown
                         try:
                             client1.wait(5)
                         except:
-                            raise RuntimeError(f"Client did not shutdown after 5sec: '{self.clientNames[0]}'")
+                            self._l.error(f"Client did not shutdown after 5sec: '{self.clientNames[0]}'")
+                            client1.kill()
+                            client1.wait()
                         try:
                             client2.wait(5)
                         except:
-                            raise RuntimeError(f"Client did not shutdown after 5sec: '{self.clientNames[1]}'")
+                            self._l.error(f"Client did not shutdown after 5sec: '{self.clientNames[1]}'")
+                            client2.kill()
+                            client2.wait()
+                        
+                        # Save client logs
+                        self.logger.logClient(0, client1.stdout)
+                        self.logger.logClient(1, client2.stdout)
+                        
+                        # Swap player scores as we swaped their reservations
+                        # (== 0 here because we already incremented self.playedGames at the top of this if)
+                        if self.playedGames % 2 == 0:
+                            msg.payload.swap()
+                        
+                        # Check ending reason
+                        def checkEnd(cause: ResultCause, client: int) -> None:
+                            """Checks the game ending cause for both players
+
+                            Arguments:
+                                cause  {ResultCause} -- Cause of game ending
+                                client {int}         -- Client id of this cause
+                            """
+                            if cause == ResultCause.REGULAR:
+                                self._l.debug(f"Client {self.clientNames[client]} ended REGULAR")
+                            if cause == ResultCause.LEFT:
+                                self._l.error(f"Client {self.clientNames[client]} LEFT the game")
+                            if cause == ResultCause.RULE_VIOLATION:
+                                self._l.critical(f"Client {self.clientNames[client]} VIOLATED the RULES")
+                            if cause == ResultCause.SOFT_TIMEOUT:
+                                self._l.error(f"Client {self.clientNames[client]} SOFT TIMEOUTED")
+                            if cause == ResultCause.HARD_TIMEOUT:
+                                self._l.error(f"Client {self.clientNames[client]} HARD TIMEOUTED")
+                        
+                        checkEnd(msg.payload.cause[0], 0)
+                        checkEnd(msg.payload.cause[1], 1)
                         
                         return msg.payload
+                    elif msg.type == MsgType.LEFT:
+                        self._l.error(f"Client crashed in game {self.playedGames}")
         
     def _resetState(self) -> None:
         """Reset own state before running new game
         """
+        # Clear Server Messages
+        while self.serverClient.receive():
+            pass
+        self.serverClient._buffer = ""
+        
+        # Clear internal state
         self._roomId = ""
         self._turn   = 0
     
@@ -76,8 +129,8 @@ class GameManager():
         """
         #? Create new game
         # Request
-        client1 = self.clientNames[self.playedGames   % 2]
-        client2 = self.clientNames[self.playedGames+1 % 2]
+        client1 = self.clientNames[self.playedGames     % 2]
+        client2 = self.clientNames[(self.playedGames+1) % 2]
         self.serverClient.send(f'<prepare gameType="swc_2021_blokus"><slot displayName="{client1}" canTimeout="true" shouldBePaused="true" /><slot displayName="{client2}" canTimeout="true" shouldBePaused="true" /></prepare>')
         
         # Handling response
