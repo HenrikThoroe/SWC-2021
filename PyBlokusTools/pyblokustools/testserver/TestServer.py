0279cf0ce79babf907534f924948c2706469308b
Rubin Raithel <33808743+Coronon@users.noreply.github.com>
2020-11-15 19:01:17 +0100

    (Scripts+TestServer) fully implement the TestServer

--- PyBlokusTools/pyblokustools/testserver/TestServer.py
+++ PyBlokusTools/pyblokustools/testserver/TestServer.py
@@ -1,13 +1,21 @@
-from typing import Optional, Tuple, List
+from typing import Optional, Callable, Tuple, List
 
+import os
+import atexit
 import tempfile
 import zipfile
 import logging
-from tqdm import tqdm # type: ignore
+import signal
+from time import sleep, time
+from functools import wraps
+from datetime import datetime, timedelta
+from subprocess import Popen, PIPE, STDOUT
+from tqdm import tqdm, trange # type: ignore
 from requests import get
 
 from .Config import Config
 from .Logger import Logger
+from .TCPClient import TCPClient
 from .GameManager import GameManager
 
 class TestServer():
@@ -17,31 +25,53 @@ class TestServer():
         clients         {Tuple[str, str]}             -- Paths to the two clients to test
         clientNames     {Tuple[str, str]}             -- Names for the two clients
         clientArguments {Tuple[List[str], List[str]]} -- Arguments to start clients with
-        logEnabled      {bool}                        -- Logging enabled
-        logLevel        {int}                         -- Log level to use (default: logging.INFO)
+        serverPort      {int}                         -- Port the server should run on    (default: 13055)
+        logEnabled      {bool}                        -- Logging enabled                  (default: True)
+        logLevel        {int}                         -- Log level to use                 (default: logging.INFO)
     """
-    def __init__(self, clients: Tuple[str, str], clientNames: Tuple[str, str], clientArguments: Tuple[List[str], List[str]], logEnabled: bool = True, logLevel: int = logging.INFO) -> None:
+    def __init__(
+        self,
+        clients         : Tuple[str, str],
+        clientNames     : Tuple[str, str],
+        clientArguments : Tuple[List[str], List[str]],
+        serverPort      : int                          = 13055,
+        logEnabled      : bool                         = True,
+        logLevel        : int                          = logging.INFO,
+    ) -> None:
         self.clients         = clients
         self.clientNames     = clientNames
         self.clientArguments = clientArguments
+        self.serverPort      = serverPort
         
         self.config = Config.load()
         
-        self.manager = GameManager(
-            serverClient    = self._fetchServer(),
-            clients         = clients,
-            clientNames     = clientNames,
-            clientArguments = clientArguments,
-            )
         self.logger = Logger(
             clientNames = clientNames,
             enabled     = logEnabled,
             level       = logLevel,
             )
-        
         logging.basicConfig(format='%(asctime)s %(levelname)s  %(message)s', datefmt='%H:%M:%S', level=logLevel)
         self._l = logging.getLogger("TestServer")
+        self._l.propagate = False
         self._l.addHandler(self.logger)
+        
+        self.serverPath = self._fetchServer()
+        
+        self.serverProc: Popen
+        self.client = self._startServer()
+        
+        self.manager = GameManager(
+            serverClient    = self.client,
+            serverPort      = self.serverPort,
+            clients         = clients,
+            clientNames     = clientNames,
+            clientArguments = clientArguments,
+            logger          = self.logger,
+            )
+        
+        self.scores = [0, 0]
+        
+        self.config.save()
     
     def _fetchServer(self) -> str:
         """Fetch the newest version of the SWC Server from github
@@ -57,11 +87,12 @@ class TestServer():
             """
             self._l.warning("Could not fetch newest SWC Server version from Github")
             
-            if not config.usable:
+            if not self.config.usable:
                 self._l.critical("No fallback for SWC Server found, exiting")
                 raise SystemExit()
             
-            return config.serverPath
+            self._l.info(f"Falling back to server version {self.config.serverVersion} from disk")
+            return self.config.serverPath
         
         self._l.info("Fetching newest SWC Server version from Github...")
         
@@ -72,12 +103,12 @@ class TestServer():
             self._l.warning("Could not reach Github")
             return fallback()
         
-        if r.status_code != 200:
+        if releases.status_code != 200:
             #? Other error
-            self._l.warning(f"Got response with status code '{r.status_code}'")
+            self._l.warning(f"Got response with status code '{releases.status_code}'")
             return fallback()
 
-        j = r.json()[0]
+        j = releases.json()[0]
         tag_name = j['tag_name']
         assets   = j['assets'] # Most current versions assets
         
@@ -95,16 +126,153 @@ class TestServer():
                             with zipfile.ZipFile(file, 'r') as serverZip:
                                 serverZip.extractall(os.path.dirname(self.config.serverPath))
                             
+                            self.config.serverHash    = asset['node_id']
+                            self.config.serverVersion = tag_name 
+                            
                             self._l.info(f"Using freshly downloaded server version {tag_name}")
                             return self.config.serverPath
                     except:
                         self._l.warning(f"Could not download server verion {tag_name}")
                         return fallback()
 
-                self._l.info(f"Using current version server version {self.config.version} from disk")
+                self._l.info(f"Using current server version {self.config.serverVersion} from disk")
                 return self.config.serverPath
                 
-        self._l.exception("Did not find 'software-challenge-server.zip' in assets")
+        self._l.error("Did not find 'software-challenge-server.zip' in assets")
         return fallback()
-                
+
+    def _startServer(self) -> TCPClient:
+        """Start the SWC Server and establish a connection to it
+
+        Returns:
+            TCPClient -- Connected and authenticated client
+        """
+        self.serverProc = Popen(
+            [self.config.serverPath, '-p', str(self.serverPort)],
+            stdout     = PIPE,
+            stderr     = STDOUT,
+            cwd        = os.path.dirname(self.config.serverPath),
+            preexec_fn = os.setsid,
+            )
         
+        def killSWCServer() -> None:
+            """Called before termination of TestServer to make sure the SWC server is shut down
+            """
+            os.killpg(os.getpgid(self.serverProc.pid), signal.SIGTERM)
+            self.serverProc.kill()
+            self.serverProc.wait()
+        
+        atexit.register(killSWCServer)
+        
+        os.set_blocking(self.serverProc.stdout.fileno(), False) # type: ignore
+        
+        self._l.info("Booting up SWC server...")
+        sleep(5)
+        
+        client = TCPClient("localhost", self.serverPort)
+        client.send('<authenticate password="examplepassword" />')
+        
+        self._l.info(f"SWC server started on port '{self.serverPort}'")        
+        return client
+    
+    def run(self, *, iterations: Optional[int] = None, deltatime: Optional[timedelta] = None) -> None:
+        """Run configured mass tests (specify either iterations or time)
+
+        Keyword Arguments:
+            iterations {Optional[int]}       -- Test runs to execute (default: {None})
+            deltatime  {Optional[timedelta]} -- Time to test for     (default: {None})
+
+        Raises:
+            TypeError: Invalid arguments
+        """
+        if iterations is None and deltatime is None:
+            raise TypeError("Please specify iterations or deltatime")
+        
+        #? Define decorator
+        if isinstance(iterations, int) and iterations > 0:
+            def _(func: Callable[[], None]) -> Callable[[], None]:
+                """Decorator to execute func x times
+
+                Arguments:
+                    func {Callable[[], None]} -- Function to repeat
+                
+                Returns:
+                    Callable[[], None] -- Repeated function
+                """
+                @wraps(func)
+                def f(*args, **kwargs) -> None:
+                    with trange(iterations, unit="Game") as t:
+                        t.set_postfix_str(f"Scores {self.scores[0]}:{self.scores[1]}")
+                        for run in t:
+                            t.set_description(f'Game {self.manager.playedGames+1}')
+                            func()
+                            t.set_postfix_str(f"Scores {self.scores[0]}:{self.scores[1]}")
+                return f            
+        elif isinstance(deltatime, timedelta) and deltatime.total_seconds() > 0:
+            def _(func: Callable[[], None]) -> Callable[[], None]:
+                """Decorator to execute func for a specific time
+
+                Arguments:
+                    func {Callable[[], None]} -- Function to repeat
+                
+                Returns:
+                    Callable[[], None] -- Repeated function
+                """
+                @wraps(func)
+                def f(*args, **kwargs) -> None:
+                    left    = int(deltatime.total_seconds()) # type: ignore
+                    elapsed = 0
+                    elapsedMean = 0
+                    
+                    with tqdm(iterations, unit="Game", total=left, bar_format="{desc} {percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining} {postfix}]") as t:
+                        while left > elapsed:
+                            t.set_postfix_str(f"{str(round(elapsedMean / self.manager.playedGames, 1))+'s' if self.manager.playedGames > 0 else '?'}/Game , Scores {self.scores[0]}:{self.scores[1]}")
+                            t.set_description(f'Game {self.manager.playedGames+1}')
+                            
+                            left -= elapsed
+                            
+                            t.update(elapsed)
+                            
+                            e1 = time()
+                            func()
+                            _ = time()-e1
+                            elapsedMean += _
+                            elapsed = int(_)
+                            
+                        t.update(left)
+                        t.set_postfix_str(f"{str(round(elapsedMean / self.manager.playedGames, 1))+'s' if self.manager.playedGames > 0 else '?'}/Game , Scores {self.scores[0]}:{self.scores[1]}")
+                        
+                return f
+        else:
+            raise TypeError("Neither iterations nor time have a valid value")
+        
+        #? Run repeated tests
+        @_
+        def _run() -> None:
+            """Function that actually runs the mass tests
+            """
+            # Prepare logger for new game
+            self.logger.makeDirs()
+            
+            # Run actual game
+            try:
+                result = self.manager.run()
+            except Exception as e:
+                self._l.critical(f"Fatal error during game {self.manager.playedGames}?", exc_info=True)
+                # Write Serverlogs to current game logs
+                self.logger.logServer(self.serverProc.stdout)
+                return
+                            
+            # Update scores and log them
+            self.scores[0] += int(result.end[0])
+            self.scores[1] += int(result.end[1])
+            self.logger.logFile(f"Finished Game {self.manager.playedGames}:\n\t{result.__repr__()}\n\tScore is now: {self.clientNames[0]}: {self.scores[0]}, {self.clientNames[1]}: {self.scores[1]}\n")
+            
+            self._l.debug(f"Finished game {self.manager.playedGames} - {result.end[0]}:{result.end[1]}")
+            
+            # Write Serverlogs to current game logs
+            self.logger.logServer(self.serverProc.stdout)
+        
+        self._l.info("Running MassTests...")
+        _run()
+        self.logger.logFile(f"\n\nFinal Scores {self.clientNames[0]}: {self.scores[0]}, {self.clientNames[1]}: {self.scores[1]}\nAfter {self.manager.playedGames} games")
