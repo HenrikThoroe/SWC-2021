962e94b60641fdb2f0290c7d1f72612545619afc
Rubin Raithel <33808743+Coronon@users.noreply.github.com>
2020-11-15 23:13:59 +0100

    (Scripts) add missing requests requirement

--- PyBlokusTools/setup.py
+++ PyBlokusTools/setup.py
@@ -20,7 +20,8 @@ setuptools.setup(
         'Sphinx>=3.2.1',
         'sphinx-rtd-theme>=0.5.0',
         'breathe>=4.22.1',
-        'tqdm>=4.51.0'
+        'tqdm>=4.51.0',
+        'requests>=2.25.0',
     ],
     entry_points = {
         "console_scripts": [
