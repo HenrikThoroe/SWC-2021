c6264acb80a12417cbe8cced89003668ed6057bf
Henrik Thoroe <56037939+HenrikThoroe@users.noreply.github.com>
2021-01-04 18:59:48 +0000

    Adapt new constructor + Remove redunant benchmark

--- app/tests/ml/Bench_DNN.cpp
+++ app/tests/ml/Bench_DNN.cpp
@@ -9,10 +9,10 @@
 
 using namespace ML;
 
-TEST_CASE("Bench DNN", "[benchmark][ml]") {
-    DNN net_4x2x1 = DNN({ 4, 2, 1 }, { ActivationFunction::Type::LEAKY_RELU, ActivationFunction::Type::SIGMOID });
-    DNN net_400x200x1 = DNN({ 400, 200, 1 }, { ActivationFunction::Type::LEAKY_RELU, ActivationFunction::Type::SIGMOID });
-    DNN net_88x44x1 = DNN({ 88, 44, 22, 1 }, { ActivationFunction::Type::LINEAR, ActivationFunction::Type::LEAKY_RELU, ActivationFunction::Type::SIGMOID });
+TEST_CASE("Bench DNN", "[benchmark][ml][net]") {
+    DNN net_4x2x1 = DNN({ 4, 2, 1 }, { ActivationFunction::Type::LEAKY_RELU, ActivationFunction::Type::SIGMOID }, true);
+    DNN net_400x200x1 = DNN({ 400, 200, 1 }, { ActivationFunction::Type::LEAKY_RELU, ActivationFunction::Type::SIGMOID }, true);
+    DNN net_88x44x1 = DNN({ 88, 44, 22, 1 }, { ActivationFunction::Type::LINEAR, ActivationFunction::Type::LEAKY_RELU, ActivationFunction::Type::SIGMOID }, true);
 
     std::vector<float> input_4 = Util::randomVector(4);
     std::vector<float> input_400 = Util::randomVector(400);
@@ -28,15 +28,5 @@ TEST_CASE("Bench DNN", "[benchmark][ml]") {
 
     BENCHMARK("[88, 44, 22, 1]") {
         return net_88x44x1.predict(input_88);
-    };
-
-    BENCHMARK("[88, 44, 22, 1] x 1000") {
-        float val = 0;
-
-        for (int i = 0; i < 1000; ++i) {
-            val += net_88x44x1.predict(input_88)[0];
-        }
-
-        return val;
     };
 }
