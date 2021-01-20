6d2a84d9c6cec41957d6d45bf3aedc4f47119ff4
Henrik <henrik@thoroee.com>
2021-01-20 18:47:33 +0100

    Add benchmark for 400 input

--- app/tests/ml/Bench_DNN.cpp
+++ app/tests/ml/Bench_DNN.cpp
@@ -13,6 +13,7 @@ TEST_CASE("Bench DNN", "[benchmark][ml][net]") {
     DNN net_4x2x1 = DNN({ 4, 2, 1 }, { ActivationFunction::Type::LEAKY_RELU, ActivationFunction::Type::SIGMOID }, true);
     DNN net_400x200x1 = DNN({ 400, 200, 1 }, { ActivationFunction::Type::LEAKY_RELU, ActivationFunction::Type::SIGMOID }, true);
     DNN net_88x44x1 = DNN({ 88, 44, 22, 1 }, { ActivationFunction::Type::LEAKY_RELU, ActivationFunction::Type::LEAKY_RELU, ActivationFunction::Type::SIGMOID }, true);
+    DNN net_400x44x22x1 = DNN({ 400, 44, 22, 1 }, { ActivationFunction::Type::LEAKY_RELU, ActivationFunction::Type::LEAKY_RELU, ActivationFunction::Type::SIGMOID }, true);
 
     std::vector<float> input_4 = Util::randomVector(4);
     std::vector<float> input_400 = Util::randomVector(400);
@@ -28,6 +29,10 @@ TEST_CASE("Bench DNN", "[benchmark][ml][net]") {
 
     BENCHMARK("[88, 44, 22, 1]") {
         return net_88x44x1.predict(input_88);
+    };
+
+    BENCHMARK("[400, 44, 22, 1]") {
+        return net_400x44x22x1.predict(input_400);
     };
 
     BENCHMARK("[400, 200, 1] Update x1") {
