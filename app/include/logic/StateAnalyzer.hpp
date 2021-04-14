b164bcbb4336e9c32165ec0b75b9a3954602d248
Henrik <henrik@thoroee.com>
2021-04-14 15:33:40 +0200

    Remove dead code + Simplify strategy

--- app/include/logic/StateAnalyzer.hpp
+++ app/include/logic/StateAnalyzer.hpp
@@ -6,6 +6,7 @@
 #include "Rect.hpp"
 #include "Player.hpp"
 #include "Position.hpp"
+#include "Board.hpp"
 
 // Forward declare GameState to prevent cross-includes
 namespace Model {
@@ -43,27 +44,14 @@ namespace Logic {
             /// Returns the area of the deployed pieces of `player`
             Util::Rect getPlayerArea(const Model::Player& player) const; 
 
-            /// Finds the center of the pieces from `player`
-            Util::Position getPlayerCenter(const Model::Player& player) const;
-
             /**
-             * Calculates a value which indicates how much a color is pulled to the opposite one.
-             * The closer the color is to this corner the higher the value.
+             * Turn independent basic strategie points. Covers general aspects like drop positions, blocked corners and piece spread.
              * 
-             * @param color The color which should be analyzed
-             * @param swapCorner If true the value is calculated as if the starting corner of the color is shifted on the y-axis
+             * @param stats The statistics of the board
+             * @param colorIdx The indecies of the player colors in stats
+             * @param opponentColorIdx The indecies of the opponent colors in stats
              */
-            int pullFactor(const Model::PieceColor& color, bool swapCorner = false) const;
-
-            int pushFactor(const Model::PieceColor& color) const;
-
-            /**
-             * Calculates a value which maximizes the number of own drop positions while minimizing the number of opponent drop positions
-             * 
-             * @param colors The colors of the maximized player
-             * @param opponentColors The colors of the minimized player
-             */
-            int dropFactor(const std::array<Model::PieceColor, 2>& colors, const std::array<Model::PieceColor, 2>& opponentColors) const;
+            int basicStrategie(const std::array<Model::BoardStatistics, 4>& stats, const std::array<int, 2> colorIdx, const std::array<int, 2> opponentColorIdx) const;
     };
 
 }
--- app/src/logic/StateAnalyzer.cpp
+++ app/src/logic/StateAnalyzer.cpp
@@ -30,72 +30,46 @@ namespace Logic {
         return getBoundingRect(player.getPieceColors()[0]).join(getBoundingRect(player.getPieceColors()[1]));
     }
 
-    Util::Position StateAnalyzer::getPlayerCenter(const Model::Player& player) const {
-        int xSum = 0;
-        int ySum = 0;
-        int vals = 0;
+    int StateAnalyzer::basicStrategie(const std::array<Model::BoardStatistics, 4>& stats, const std::array<int, 2> colorIdx, const std::array<int, 2> opponentColorIdx) const {
+        int spread = 0;
+        int drop = 0;
+        int block = 0;
 
-        for (const std::optional<Model::DeployedPiece>& piece : gameState->getMoveHistory()) {
-            if (piece.has_value() && (piece.value().color == player.getPieceColors()[0] || piece.value().color == player.getPieceColors()[1])) {
-                for (const Util::Position& pos : piece.value().getOccupiedPositions()) {
-                    vals += 1;
-                    xSum += static_cast<int>(pos.x);
-                    ySum += static_cast<int>(pos.y);
-                }
-            }
+        //* Drop 
+
+        drop += stats[colorIdx[0]].dropPositions * 8;
+        drop += stats[colorIdx[1]].dropPositions * 8;
+
+        drop -= stats[opponentColorIdx[0]].dropPositions * 8;
+        drop -= stats[opponentColorIdx[1]].dropPositions * 8;
+
+        for (int i = 0; i < 8; ++i) {
+            drop += stats[colorIdx[0]].ratedDropPositions[i] * i;
+            drop += stats[colorIdx[1]].ratedDropPositions[i] * i;
+
+            drop -= stats[opponentColorIdx[0]].ratedDropPositions[i] * i;
+            drop -= stats[opponentColorIdx[1]].ratedDropPositions[i] * i;
         }
 
-        return Util::Position(xSum / vals, ySum / vals);
-    }
+        //* Block
 
-    int StateAnalyzer::pullFactor(const Model::PieceColor& color, bool swapCorner) const {
-        int pull = 0;
-        const int startX = gameState->startPositions[static_cast<uint8_t>(color) - 1][0];
-        int startY = gameState->startPositions[static_cast<uint8_t>(color) - 1][1];
+        //? Penalty when the team is blocking itself
 
-        if (swapCorner) {
-            startY += startY > 0 ? -19 : 19;
-        }
+        block -= stats[colorIdx[0]].friendlyBlockedCorners * 4;
+        block -= stats[colorIdx[1]].friendlyBlockedCorners * 4;
 
-        for (const std::optional<Model::DeployedPiece>& piece : gameState->getMoveHistory()) {
-            if (piece.has_value() && piece.value().color == color) {
-                for (const Util::Position& pos : piece.value().getOccupiedPositions()) {
-                    const int x = static_cast<int>(pos.x) - startX;
-                    const int y = static_cast<int>(pos.y) - startY;
+        //? Block the colors of the opponent
 
-                    pull = std::max(pull, std::min(abs(x), abs(y)));
-                }
-            }
-        }
+        block += stats[opponentColorIdx[0]].opponentBlockedCorners;
+        block += stats[opponentColorIdx[1]].opponentBlockedCorners;
 
-        return pull;
-    }
+        //* Spread 
 
-    int StateAnalyzer::dropFactor(const std::array<Model::PieceColor, 2>& colors, const std::array<Model::PieceColor, 2>& opponentColors) const {
-        const int ownPositions = gameState->getBoard().estimateDropPositions(colors[0], colors[1]);
-        const int opponentPositions = gameState->getBoard().estimateDropPositions(opponentColors[0], opponentColors[1]);
+        spread += stats[colorIdx[0]].teamPushFactor + stats[colorIdx[1]].teamPushFactor;
+        spread -= stats[opponentColorIdx[0]].pushFactor + stats[opponentColorIdx[1]].pushFactor;
 
-        return ownPositions - 2 * opponentPositions;
-    }
-
-    int StateAnalyzer::pushFactor(const Model::PieceColor& color) const {
-        int push = 0;
-        const int startX = gameState->startPositions[static_cast<uint8_t>(color) - 1][0];
-        int startY = gameState->startPositions[static_cast<uint8_t>(color) - 1][1];
-
-        for (const std::optional<Model::DeployedPiece>& piece : gameState->getMoveHistory()) {
-            if (piece.has_value() && piece.value().color == color) {
-                for (const Util::Position& pos : piece.value().getOccupiedPositions()) {
-                    const int x = static_cast<int>(pos.x) - startX;
-                    const int y = static_cast<int>(pos.y) - startY;
-
-                    push += std::max(abs(x), abs(y));
-                }
-            }
-        }
-
-        return push;
-    }
+        return spread + block + drop;
+    }   
 
     int StateAnalyzer::strategyPoints(const std::array<Model::PieceColor, 2>& colors, const std::array<Model::PieceColor, 2>& opponentColors) const {
         //? Prepare information
@@ -107,20 +81,16 @@ namespace Logic {
             gameState->startPositions[2][0] + 20 * gameState->startPositions[2][1],
             gameState->startPositions[3][0] + 20 * gameState->startPositions[3][1]
         };
+
         bool horizontal =
             static_cast<int>(board.at(0, 0)) >= 3                       // If top left is team two
-            ? ( static_cast<int>(board.at(0, 19)) >= 3 ? true : false)  // YES: If bottom left is team two -> horizontal
-            : ( static_cast<int>(board.at(0, 19)) >= 3 ? false : true); // NO: If bottom left is team two -> vertical
+            ? (static_cast<int>(board.at(0, 19)) >= 3 ? true : false)   // YES: If bottom left is team two -> horizontal
+            : (static_cast<int>(board.at(0, 19)) >= 3 ? false : true);  // NO: If bottom left is team two -> vertical
 
         const std::array<Model::BoardStatistics, 4>& stats = board.getStatistics(horizontal);
         const std::array<int, 2> colorIdx = { static_cast<int>(colors[0]) - 1, static_cast<int>(colors[1]) - 1 };
         const std::array<int, 2> opponentColorIdx = { static_cast<int>(opponentColors[0]) - 1, static_cast<int>(opponentColors[1]) - 1 };
-        const std::array<Util::Rect, 4> bounds = {
-            getBoundingRect(static_cast<Model::PieceColor>(1)),
-            getBoundingRect(static_cast<Model::PieceColor>(2)),
-            getBoundingRect(static_cast<Model::PieceColor>(3)),
-            getBoundingRect(static_cast<Model::PieceColor>(4))
-        };
+
         std::array<int, 4> startIndex {};
         std::array<int, 4> opposingNeighbour{};
         std::array<int, 4> oppositeCorner{};
@@ -160,81 +130,13 @@ namespace Logic {
             }
         }
 
-        //* Start 
-
-        if (gameState->getTurn() < 16) {
+        if (gameState->getTurn() < 20) {
             //? Push each color to the middle 
 
             return stats[colorIdx[0]].pullFactor[oppositeCorner[colorIdx[0]]] + stats[colorIdx[1]].pullFactor[oppositeCorner[colorIdx[1]]];
         }
 
-        //* Mid
-
-        if (gameState->getTurn() < 50) {
-            int drop = 0;
-            int size = 0;
-            int block = 0;
-            int dir = 0;
-
-            dir += stats[colorIdx[0]].pullFactor[opposingNeighbour[colorIdx[0]]];
-            dir += stats[colorIdx[1]].pullFactor[opposingNeighbour[colorIdx[1]]];
-
-            dir -= stats[opponentColorIdx[0]].pullFactor[opposingNeighbour[opponentColorIdx[0]]] * 2;
-            dir -= stats[opponentColorIdx[1]].pullFactor[opposingNeighbour[opponentColorIdx[1]]] * 2;
-
-            size -= bounds[opponentColorIdx[0]].size();
-            size -= bounds[opponentColorIdx[1]].size();
-
-            drop += stats[colorIdx[0]].dropPositions;
-            drop += stats[colorIdx[1]].dropPositions;
-
-            block += stats[opponentColorIdx[0]].opponentBlockedCorners * 100;
-            block += stats[opponentColorIdx[1]].opponentBlockedCorners * 100;
-
-            for (int i = 0; i < 8; ++i) {
-                drop -= stats[opponentColorIdx[0]].ratedDropPositions[i] * i * 4;
-                drop -= stats[opponentColorIdx[1]].ratedDropPositions[i] * i * 4;
-            }
-
-            return drop + size + block + dir;
-        }
-
-        //* Late
-
-        int drop = 0;
-        int size = 0;
-        int block = 0;
-        int dir = 0;
-
-        //? Penalty when the team is blocking itself
-
-        block -= stats[colorIdx[0]].friendlyBlockedCorners;
-        block -= stats[colorIdx[1]].friendlyBlockedCorners;
-
-        //? Block the colors of the opponent
-
-        block += stats[opponentColorIdx[0]].opponentBlockedCorners * 4;
-        block += stats[opponentColorIdx[1]].opponentBlockedCorners * 4;
-
-        //? Maximize own drop positions while minimizing opponent drop positions
-
-        for (int i = 0; i < 8; ++i) {
-            drop += stats[colorIdx[0]].ratedDropPositions[i] * i;
-            drop += stats[colorIdx[1]].ratedDropPositions[i] * i;
-
-            drop -= stats[opponentColorIdx[0]].ratedDropPositions[i] * i;
-            drop -= stats[opponentColorIdx[1]].ratedDropPositions[i] * i;
-        }
-
-        //? Push own color to the opposite side and prevent the same from the opponent
-
-        dir -= stats[opponentColorIdx[0]].alignment[startSide[opponentColorIdx[0]]];
-        dir -= stats[opponentColorIdx[1]].alignment[startSide[opponentColorIdx[1]]];
-
-        dir += stats[colorIdx[0]].alignment[startSide[colorIdx[0]]];
-        dir += stats[colorIdx[1]].alignment[startSide[colorIdx[1]]];
-
-        return drop + size + block + dir;
+        return basicStrategie(stats, colorIdx, opponentColorIdx);
     }
 
 }
